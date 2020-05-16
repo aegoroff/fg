db62895a5e8c1707fdfc5333b83178a96e16fe0c
egoroff <egoroff@gmail.com>
2020-05-16 13:52:47 +0300

    similar tests merged

--- fg_test.go
+++ fg_test.go
@@ -10,56 +10,45 @@ import (
 
 const fgTestFileName = "file.txt"
 
-func TestFilterFile_OnlyIncludeSetFileMatchPattern_ReturnFalse(t *testing.T) {
-	// Arrange
-	ass := assert.New(t)
-	include := "*.txt"
-	var exclude string
+func TestFilterFile_OnlyOneOptionSetFileNotMatchPattern_ReturnFalse(t *testing.T) {
+	var tests = []struct {
+		include string
+		exclude string
+	}{
+		{"*.txt", ""},
+		{"", "*.mov"},
+	}
 
-	// Act
-	result := filterFile(fgTestFileName, include, exclude)
+	for _, test := range tests {
+		// Arrange
+		ass := assert.New(t)
 
-	// Assert
-	ass.Falsef(result, "File name %s should not be filtered by %s but it was", fgTestFileName, include)
+		// Act
+		result := filterFile(fgTestFileName, test.include, test.exclude)
+
+		// Assert
+		ass.Falsef(result, "File name %s should not be filtered but it was", fgTestFileName)
+	}
 }
 
-func TestFilterFile_OnlyIncludeSetFileNotMatchPattern_ReturnTrue(t *testing.T) {
-	// Arrange
-	ass := assert.New(t)
-	include := "*.mov"
-	var exclude string
+func TestFilterFile_OnlyOneOptionSetAndItFiltersFile_ReturnTrue(t *testing.T) {
+	var tests = []struct {
+		include string
+		exclude string
+	}{
+		{"*.mov", ""},
+		{"", "*.txt"},
+	}
+	for _, test := range tests {
+		// Arrange
+		ass := assert.New(t)
 
-	// Act
-	result := filterFile(fgTestFileName, include, exclude)
+		// Act
+		result := filterFile(fgTestFileName, test.include, test.exclude)
 
-	// Assert
-	ass.Truef(result, "File name %s should be filtered by %s but it wasn't", fgTestFileName, include)
-}
-
-func TestFilterFile_OnlyExcludeSetFileMatchPattern_ReturnTrue(t *testing.T) {
-	// Arrange
-	ass := assert.New(t)
-	var include string
-	exclude := "*.txt"
-
-	// Act
-	result := filterFile(fgTestFileName, include, exclude)
-
-	// Assert
-	ass.Truef(result, "File name %s should be filtered by %s but it wasn't", fgTestFileName, include)
-}
-
-func TestFilterFile_OnlyExcludeSetFileNotMatchPattern_ReturnFalse(t *testing.T) {
-	// Arrange
-	ass := assert.New(t)
-	var include string
-	exclude := "*.mov"
-
-	// Act
-	result := filterFile(fgTestFileName, include, exclude)
-
-	// Assert
-	ass.Falsef(result, "File name %s should not filtered by %s but it was", fgTestFileName, include)
+		// Assert
+		ass.Truef(result, "File name %s should be filtered but it wasn't", fgTestFileName)
+	}
 }
 
 func TestFilterFile_BothPatternsEmpty_ReturnFalse(t *testing.T) {
