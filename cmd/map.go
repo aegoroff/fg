7f0345068a20a46b5f38df32e01efed11e08f208
egoroff <egoroff@gmail.com>
2020-05-18 12:55:39 +0300

    golint fixes

--- cmd/map.go
+++ cmd/map.go
@@ -3,7 +3,7 @@ package cmd
 // Defines string keys map
 type StringKeyMap map[string]interface{}
 
-// Gets all map's keys
+// ContainsKey gets all map's keys
 func (m *StringKeyMap) Keys() []string {
 	keys := make([]string, 0, len(*m))
 	for k := range *m {
@@ -12,7 +12,7 @@ func (m *StringKeyMap) Keys() []string {
 	return keys
 }
 
-// Gets whether a key is presented in map
+// ContainsKey gets whether a key is presented in map
 func (m *StringKeyMap) ContainsKey(key string) bool {
 	_, ok := (*m)[key]
 	return ok
