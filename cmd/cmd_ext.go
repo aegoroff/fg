beb812ffb52e6ba124a2296a33c0dcef036299d3
egoroff <egoroff@gmail.com>
2022-04-04 00:33:05 +0300

    refactor: Extension grouping using path module instead of string operations

--- cmd/cmd_ext.go
+++ cmd/cmd_ext.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"os"
+	"path"
 	"strings"
 )
 
@@ -10,17 +11,16 @@ func newExt(c conf) *cobra.Command {
 	cmd := &simple{
 		use:   "ext",
 		a:     "e",
-		descr: "Group files by file extensio",
+		descr: "Group files by file extension",
 		g:     extGrouper,
 	}
 	return newSimpleGroupingCmd(c, cmd)
 }
 
 func extGrouper(file os.FileInfo) []string {
-	name := file.Name()
-	parts := strings.Split(name, ".")
-	if len(parts) < 2 {
+	ext := path.Ext(file.Name())
+	if len(ext) == 0 {
 		return []string{"no extension"}
 	}
-	return []string{parts[len(parts)-1]}
+	return []string{strings.TrimPrefix(ext, ".")}
 }
